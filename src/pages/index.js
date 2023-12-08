11badd1027d87c804297199c8becfed17f743c3a
shakogegia <shalva.gegia@gmail.com>
2023-12-08 15:09:13 +0100

    style: home page layout padding

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { input } from '@/utils/constants'
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col items-center gap-24 p-24`}>
+    <main className={`flex min-h-screen flex-col items-center gap-24 p-14`}>
       <h1 className="text-3xl font-serif">E-Book Reader</h1>
 
       <Reader input={input} />
